c3ae7005d3fbe2fe2d86406ab609aa3e4ff4059f
Kyle-Helmick <helmick.kyle@gmail.com>
2019-02-10 00:31:21 -0700

    Refactor App.js to contain Routing, and pass tests.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import "../styles/App.css";
 
 import Landing from "./slides/Landing";
@@ -9,11 +9,13 @@ import Register from "./Register";
 class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route path="/signin" component={Signin} />
-        <Route path="/register" component={Register} />
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/signin" component={Signin} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
