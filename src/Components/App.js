e0ede23339012e61a2c64f1650a4765b26b283a5
Kyle-Helmick <helmick.kyle@gmail.com>
2019-04-12 01:00:43 -0600

    OUR FIRST WORKING MUTATION createUser

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import "../styles/App.css";
+
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink } from "apollo-link";
 
 import Landing from "./slides/Landing";
 import Signin from "./Signin";
@@ -11,7 +16,22 @@ import Profile from "./Profile";
 import GroceryList from "./GroceryList";
 
 const client = new ApolloClient({
-  uri: "http://localhost:3000/graphql"
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: "http://localhost:3000/graphql",
+      credentials: "include"
+    })
+  ]),
+  cache: new InMemoryCache()
 });
 
 class App extends Component {
